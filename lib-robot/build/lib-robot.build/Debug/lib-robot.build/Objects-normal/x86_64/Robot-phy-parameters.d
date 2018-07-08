941311c2ba6b9056f5f047651276738f64835bab
Melanie Jouaiti <melanie.jouaiti@gmail.com>
2018-07-08 14:21:32 +0200

    working on Linux

--- lib-robot/build/lib-robot.build/Debug/lib-robot.build/Objects-normal/x86_64/Robot-phy-parameters.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/Robot-phy-parameters.cpp \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/Robot.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../KinovaTypes.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../Kinova.API.CommLayerUbuntu.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../Kinova.API.UsbCommandLayerUbuntu.h
--- lib-robot/build/lib-robot.build/Debug/lib-robot.build/Objects-normal/x86_64/Robot-time.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/Robot-time.cpp \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/Robot.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../KinovaTypes.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../Kinova.API.CommLayerUbuntu.h \
-  /Users/Melanie/Documents/Studies/LORIA/Code/kinovaWrapper/lib-robot/../Kinova.API.UsbCommandLayerUbuntu.h
